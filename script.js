a29ed37d4a872fff97c4b130a4515afbc61ea0dc
cjdepo <cjdepowell@protonmail.com>
2021-09-12 19:49:56 -0400

    Added buttons and event listener

--- script.js
+++ script.js
@@ -11,58 +11,63 @@ function computerPlay () {
     
 }
 
-function playerPlay () {
-    player = window.prompt('Play your hand: ');
-    playerSelection = player.toLowerCase();
-    return(player);
-}
 
-function playRound () {
-    player = playerPlay();
+function playRound (play) {
+    player = play;
+    console.log(player);
     computer = computerPlay();
+    console.log(computer);
     if (player == computer) {
         console.log("Tie!")
-        return "tie";
+        //return "tie";
     } else if ((player == "rock" && computer == "scissor") | (player == "scissor" && computer == "paper") | (player == "paper" && computer == "rock")) {
         console.log("Win Round!")
-        return "win";
+        //return "win";
     } else if ((computer == "rock" && player == "scissor") | (computer == "scissor" && player == "paper") | (computer == "paper" && player == "rock")) {
         console.log("Lose Round!")
-        return "lose";
+        //return "lose";
     } else {
         console.log("ERROR, try again. (rock/paper/scissor)");
     }
 
     }    
    
-function game(numgames) {
+function game(numwins) {
     score = 0;
     games = 0;
     playerscore = 0;
     computerscore = 0;
-    while (games <= numgames) {
-       round = playRound();
+    while (computerscore <= numwins && playerscore <= numwins) {
+        // round =
        if (round == "win") {
            games += 1;
            playerscore += 1;
        } else if (round == "lose") {
            games += 1;
-           computerscore += 1
+           computerscore += 1;
        } else if (round == "tie") {
 
        }
        
-       if (playerscore >= ((numgames + 1)/2)) {
+       if (playerscore >= numwins) {
            console.log("WIN GAME !!!");
            break;
-       } else if (computerscore >= ((numgames + 1)/2)) {
+       } else if (computerscore >= numwins) {
             console.log("LOSE GAME !!!");
             break;
         } else {
             continue;
         }
+        
     }
 }    
 
-test = game(5);
+
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach( (button) => button.addEventListener('click', function() {playRound(button.id)}));
+
+
+// test = game(5);
 
